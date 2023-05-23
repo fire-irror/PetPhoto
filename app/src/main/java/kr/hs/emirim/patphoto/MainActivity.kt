9104d100455491eb4e9f7b65c1c1a1f535497396
fire-irror <firenoah.m@gmail.com>
2023-05-23 20:01:51 +0900

    직접 풀어보기 (4-3)완성

--- app/src/main/java/kr/hs/emirim/patphoto/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/patphoto/MainActivity.kt
@@ -1,9 +1,11 @@
 package kr.hs.emirim.patphoto
 
 import android.annotation.SuppressLint
+import android.location.GnssAntennaInfo.Listener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnClickListener
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,32 +14,45 @@ class MainActivity : AppCompatActivity() {
     lateinit var linear:LinearLayout
     lateinit var btnDone:Button
     lateinit var imgv:ImageView
+    lateinit var switchStart : Switch
+    lateinit var btnexit : Button
+
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        checkStart = findViewById(R.id.check_start)
+        switchStart = findViewById(R.id.switch_start)
         rg = findViewById(R.id.rg)
         linear = findViewById(R.id.linear)
         btnDone = findViewById(R.id.btn_done)
         imgv = findViewById(R.id.imgv)
+        btnexit = findViewById(R.id.btn_exit)
         linear.visibility = View.INVISIBLE
 
-        checkStart.setOnCheckedChangeListener{compoundButton, b ->
-            if(checkStart.isChecked){
+        switchStart.setOnCheckedChangeListener { compoundButton, b ->
+            if (switchStart.isChecked) {
                 linear.visibility = View.VISIBLE
-            }else{
+            } else {
                 linear.visibility = View.INVISIBLE
             }
         }
-        btnDone.setOnClickListener {
+        rg.setOnCheckedChangeListener{compoundButton, b->
             when(rg.checkedRadioButtonId){
                 R.id.radio_dog -> imgv.setImageResource(R.drawable.dog)
-                R.id.radio_cat -> imgv.setImageResource(R.drawable.cat)
                 R.id.radio_rabbit -> imgv.setImageResource(R.drawable.rab)
-                else -> Toast.makeText(applicationContext, "check a radio button", Toast.LENGTH_SHORT).show()
+                R.id.radio_cat -> imgv.setImageResource(R.drawable.cat)
+            }
+        }
+        btnDone.setOnClickListener(btnListener)
+        btnexit.setOnClickListener(btnListener)
+    }
+
+
+        var btnListener = OnClickListener{
+            when(it.id){
+                R.id.btn_done -> finish()
+                R.id.btn_exit -> linear.visibility = View.INVISIBLE
             }
         }
     }
-}
